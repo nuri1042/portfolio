22e4cce728004a5e94840c975722794f583652df
nuri1042 <rlasnfl7288@naver.com>
2021-10-12 23:38:35 +0900

    JS : Project filtering

--- main.js
+++ main.js
@@ -49,6 +49,30 @@ $arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// Project filtering
+const $workCategory = document.querySelector('.work__categories');
+const $workProjects = document.querySelector('.work__projects');
+const $projects = document.querySelectorAll('.project');
+
+$workCategory.addEventListener('click', (event) => {
+  const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (!filter) {
+    return;
+  }
+  $workProjects.classList.add('anim-out');
+
+  setTimeout(() => {
+    $projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    $workProjects.classList.remove('anim-out');
+  }, 300);
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
